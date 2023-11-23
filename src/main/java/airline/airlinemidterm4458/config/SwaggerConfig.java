6d4a3e888cab8dd5a96a1ab58267122b3693eeaa
Eylül Özatman <eylulozatman@gmail.com>
2023-11-24 01:27:42 +0300

    new-commit

--- src/main/java/airline/airlinemidterm4458/config/SwaggerConfig.java
+++ src/main/java/airline/airlinemidterm4458/config/SwaggerConfig.java
@@ -1,8 +1,15 @@
 package airline.airlinemidterm4458.config;
 
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.Collections;
 
 @Configuration
 public class SwaggerConfig {
@@ -16,6 +23,24 @@ public class SwaggerConfig {
                 .group("custom-api")
                 .pathsToMatch("/api/**")
                 .build();
+    }
+
+    @Bean
+    public OpenAPI openApiConfiguration() {
+        return new OpenAPI()
+                .components(new Components()
+                        .addSecuritySchemes("bearer-key", new SecurityScheme()
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")
+                        )
+                )
+                .info(new Info()
+                        .title("AIRLİNE PROJECT")
+                        .version("1.0.0")
+                        .description("Project Api")
+                );
+
     }
 
 }
